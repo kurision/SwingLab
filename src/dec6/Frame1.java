19f9fa2280567df2e887f894cc88a564e32d5ef3
Kurision <kumalanil34@gmail.com>
2022-12-07 09:11:21 +0545

    second commit

--- src/dec6/Frame1.java
+++ src/dec6/Frame1.java
@@ -1,2 +1,20 @@
-package dec6;public class Frame1 {
+package dec6;
+import javax.swing.*;
+//public class Frame1 {
+//    JFrame f;
+//    public Frame1(){
+//        f = new JFrame();
+//        f.setTitle("First Frame");
+//        f.setSize(600,600);
+//        f.setVisible(true);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    }
+//}
+public class Frame1 extends JFrame{
+    public Frame1(){
+        setTitle("First Frame");
+        setSize(600,600);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
