f732f794375eee404783a0aa56f8c4dcbc6454d4
Kurision <kumalanil34@gmail.com>
2022-12-21 09:35:36 +0545

    Added textfield and tooltip demo

--- src/dec21/WindowEventDemo.java
+++ src/dec21/WindowEventDemo.java
@@ -5,8 +5,13 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class WindowEventDemo extends JFrame implements WindowListener {
+    JTextField txt = new JTextField("Enter text here:");
     public WindowEventDemo(){
         addWindowListener(this);
+        setLayout(null);
+        txt.setBounds(50,50,100,80);
+        txt.setToolTipText("Wrong");
+        add(txt);
         setSize(400,400);
         setVisible(true);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
