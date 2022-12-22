02b7e42b0b3c11163f6765ce85b7780a5f9c0e72
Kurision <kumalanil34@gmail.com>
2022-12-22 09:37:18 +0545

    Added mouse motion listener example

--- src/dec22/MouseEventEx.java
+++ src/dec22/MouseEventEx.java
@@ -4,21 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
-public class MouseEventEx extends MouseAdapter {
+public class MouseEventEx extends MouseMotionAdapter {
     JFrame f = new JFrame("Mouse event Demo");
     public MouseEventEx(){
         f.setLayout(new FlowLayout(FlowLayout.CENTER));
-        f.addMouseListener(this);
+        //f.addMouseListener(this);
+        f.addMouseMotionListener(this);
         f.setSize(400,300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
+//    @Override
+//    public void mouseClicked(MouseEvent e){
+//        System.out.println("Mouse is clicked");
+//        Graphics g = f.getGraphics();
+//        g.setColor(Color.lightGray);
+//        g.fillOval(e.getX(),e.getY(),10,20);
+//    }
     @Override
-    public void mouseClicked(MouseEvent e){
-        System.out.println("Mouse is clicked");
+    public void mouseDragged(MouseEvent e){
+        System.out.println("Mouse is dragged");
         Graphics g = f.getGraphics();
-        g.setColor(Color.lightGray);
+        g.setColor(Color.BLACK);
         g.fillOval(e.getX(),e.getY(),10,20);
     }
 }
